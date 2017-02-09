feb67c7f37d828d7ae2c0f2343c4d748788bb840
p15226811 <p15226811@GWHXX682WIN006W>
2017-02-09 17:15:12 +0000

    creation of clshotel + test class

---
+++ Test Framework/tstHotel.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Class_Library;
+
+namespace Test_Framework
+{
+    /// <summary>
+    /// Summary description for tstHotel
+    /// </summary>
+    [TestClass]
+    public class tstHotel
+    {
+        public tstHotel()
+        {
+            //
+            // TODO: Add constructor logic here
+            //
+        }
+
+        #region Additional test attributes
+        //
+        // You can use the following additional attributes as you write your tests:
+        //
+        // Use ClassInitialize to run code before running the first test in the class
+        // [ClassInitialize()]
+        // public static void MyClassInitialize(TestContext testContext) { }
+        //
+        // Use ClassCleanup to run code after all tests in a class have run
+        // [ClassCleanup()]
+        // public static void MyClassCleanup() { }
+        //
+        // Use TestInitialize to run code before running each test 
+        // [TestInitialize()]
+        // public void MyTestInitialize() { }
+        //
+        // Use TestCleanup to run code after each test has run
+        // [TestCleanup()]
+        // public void MyTestCleanup() { }
+        //
+        #endregion
+
+        [TestMethod]
+        public void InstanceOK()
+        {
+            clsHotel AnHotel = new clsHotel(); //create an instance of our class clsHotel
+            Assert.IsNotNull(AnHotel); //check to see the class is not null
+        }
+    }
+}
